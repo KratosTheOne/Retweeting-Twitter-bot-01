7c53717073636e4e0c5c8f0446464416f2121f83
Kaustubh Sinha <kaustubhsinha2001@gmail.com>
2020-12-08 23:30:01 +0530

    changes

--- keys.py
+++ keys.py
@@ -1,9 +1,9 @@
 # All the Consumer keys and Access tokens are stored here.
 # These Keys and Tokens are to be imported in the main file.
 
-C_K = 'AhArXbCRexNtLfUu7IluJ1Ju6'
-C_S = 'Lj0oZfizdXzkl3ct3JrzmMCZHsKYuaQPS2yr2yj0m1qFVk0AYP'
-A_K = '950392145659088896-UqvR0XKPGz6ZNWyBsm0O1ksBt5eH72q'
-A_S = 'JpDmeX4TUkwu3EIz2bJ16vGTzeq1prHCrNk1S2swHMtpU'
+C_K = 'aRtMaejLLjZI5R1Zv876lw4o3'
+C_S = 'K1PQGqQWm0X3UVXKpaS99dkFtVRVaTIniACjMz2nvbN39i7I59'
+A_K = '950392145659088896-xXlNFMnWWYdGneQOMj0yJ9cX1P7ymW3'
+A_S = 'amvdEk1Z1vN8YFYhNUQYxfkV3gSrRhcfmuCkdVbrfDNxi'
 
 
