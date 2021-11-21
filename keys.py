4424f9f26499682cefe9171fb0b9d9b4aa063696
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2021-11-22 03:06:24 +0530

    Delete keys.py

--- keys.py
+++
@@ -1,9 +0,0 @@
-# All the Consumer keys and Access tokens are stored here.
-# These Keys and Tokens are to be imported in the main file.
-
-C_K = 'aRtMaejLLjZI5R1Zv876lw4o3'
-C_S = 'K1PQGqQWm0X3UVXKpaS99dkFtVRVaTIniACjMz2nvbN39i7I59'
-A_K = '950392145659088896-xXlNFMnWWYdGneQOMj0yJ9cX1P7ymW3'
-A_S = 'amvdEk1Z1vN8YFYhNUQYxfkV3gSrRhcfmuCkdVbrfDNxi'
-
-
